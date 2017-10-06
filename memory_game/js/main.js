75afd3a450cb11249c696008abfab0d4e8192fe1
Julian Barthold <julianbarthold@Julians-Air.hsd1.ma.comcast.net>
2017-10-06 16:57:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,26 @@
 
 
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped: " + cardOne);
+console.log("User Flipped: " + cardTwo);
+
+
+if (cardsInPlay.length === 2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry try again!");
+	}
+}
+
