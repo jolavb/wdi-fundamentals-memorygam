a78cd43404dc24520da86d37c9b25734f2a72d24
Julian Barthold <julianbarthold@Julians-Air.hsd1.ma.comcast.net>
2017-10-07 17:42:08 -0400

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,28 +23,39 @@ var cards = [{
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	if (cardsInPlay.length === 2){
-		if(cardsInPlay[0]===cardsInPlay[1]){
-			alert("You found a match!");
-		}
-		else{
-			alert("Sorry try again!");
-		}
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry try again!");
 	}
 }
 
 
-
-var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank)
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
 	checkForMatch();
+	};
 }
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+	for(i=0;i<=cards.length -1;i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('gameboard').appendChild(cardElement);
+	}
+
+}
+
+createBoard();
+
+
 
 
 
